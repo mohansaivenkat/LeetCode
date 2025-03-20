42fe32663a08669438f8c1146cb8ad1d33eba9ea
Mohan Sai Venkat Vemuluri <mohansaivenkat2004@gmail.com>
2025-03-20 19:36:54 +0530

    Time: 0 ms (100%), Space: 20.7 MB (27.69%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int c : nums){
-            mp[c]++;
-        }
+        // unordered_map<int,int> mp;
+        // for(int c : nums){
+        //     mp[c]++;
+        // }
 
-        for(auto it : mp){
-            if(it.second==1){
-                return it.first;
-            }
+        // for(auto it : mp){
+        //     if(it.second==1){
+        //         return it.first;
+        //     }
+        // }
+        // return 0;
+        int res=0;
+        for(int c : nums){
+            res=res^c;
         }
-        return 0;
+        return res;
     }
     
 };
