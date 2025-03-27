c759fd7099ffb7bad841bba4f8019093a988dbfb
Mohan Sai Venkat Vemuluri <mohansaivenkat2004@gmail.com>
2025-03-27 14:53:21 +0530

    Time: 31 ms (60.25%), Space: 98.6 MB (47.95%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        int dom=0;
+
+        for(int c:nums){
+            mp[c]++;
+            if(mp[c]>n/2){
+                dom=c;
+            }
+        }
+        int f = mp[dom];
+        cout<<dom<<endl;
+        cout<<f;
+
+        int split = -1;
+
+        int f1=0;
+        
+
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==dom){
+                f1++;
+            }
+            int size1=i+1;
+            int size2=n-size1;
+            int f2=f-f1;
+            if(f1>size1/2 && f2>size2/2){
+                split=i;
+                return split;
+            }
+
+        }
+        return split;
+
+    }
+};
