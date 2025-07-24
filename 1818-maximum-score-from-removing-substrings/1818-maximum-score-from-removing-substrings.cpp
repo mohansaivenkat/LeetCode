fa737f3912f2701129da82efcab2221423f0b7d4
Mohan Sai Venkat Vemuluri <mohansaivenkat2004@gmail.com>
2025-07-24 16:05:35 +0530

    Time: 49 ms (57.87%), Space: 31.4 MB (29.57%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -4,41 +4,46 @@ using namespace std;
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        return (x > y) ? solve(s, "ab", x, "ba", y) : solve(s, "ba", y, "ab", x);
+        return (x>y) ? calculate(s,"ab",x,"ba",y) : calculate(s,"ba",y,"ab",x);
     }
 
-    int solve(string s, string firstPair, int firstVal, string secondPair, int secondVal) {
-        stack<char> st;
+    int calculate(string original,string firstpair,int first, string secondpair,int second){
+        // first pass
+        stack<char> stk;
         int count = 0;
-
-    
-        for (char c : s) {
-            if (!st.empty() && st.top() == firstPair[0] && c == firstPair[1]) {
-                st.pop();
-                count += firstVal;
-            } else {
-                st.push(c);
+        if(original.size()<2) return 0;
+        for(char c : original){
+            if(!stk.empty() and stk.top()==firstpair[0] and c==firstpair[1]){
+                stk.pop();
+                count += first;
+            }
+            else{
+                stk.push(c);
             }
         }
 
-        
-        string remaining = "";
-        while (!st.empty()) {
-            remaining += st.top();
-            st.pop();
+        //second pass
+        string remstr = "";
+        while(!stk.empty()){
+            remstr += stk.top();
+            stk.pop();
         }
-        reverse(remaining.begin(), remaining.end());
 
-    
-        for (char c : remaining) {
-            if (!st.empty() && st.top() == secondPair[0] && c == secondPair[1]) {
-                st.pop();
-                count += secondVal;
-            } else {
-                st.push(c);
+        reverse(remstr.begin(),remstr.end());
+
+        if(remstr.size()<2) return count;
+
+        for(char c: remstr){
+            if(!stk.empty() and stk.top()==secondpair[0] and c==secondpair[1]){
+                count += second;
+                stk.pop();
+            }
+            else{
+                stk.push(c);
             }
         }
 
         return count;
+
     }
 };
