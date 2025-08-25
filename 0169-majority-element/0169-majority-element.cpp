c88b2ee6d622dc33cf75753a11c95b4e5b7908a3
Mohan Sai Venkat Vemuluri <mohansaivenkat2004@gmail.com>
2025-08-26 02:50:38 +0530

    Time: 0 ms (100%), Space: 28 MB (98.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,27 +1,18 @@
-#include<vector>
-// #include<algorithms>
-using namespace std;
-
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-
         int freq=0;
-        int ans=nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]== nums[i-1]){
+        int ans=0;
+        for(int c :nums){
+            if(freq==0){
+                ans = c;
+            }
+            if(ans==c){
                 freq++;
             }
             else{
-                freq=0;
-                ans=nums[i];
+                freq--;
             }
-
-            if(freq >= nums.size()/2){
-                return ans;
-            }
-
         }
         return ans;
     }
