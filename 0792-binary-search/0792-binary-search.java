8aef2a6aaf604c1eebd6734d6192b409ac3c6742
Mohan Sai Venkat Vemuluri <mohansaivenkat2004@gmail.com>
2025-10-21 02:07:24 +0530

    Time: 0 ms (100%), Space: 45.3 MB (98.4%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int rec(int i,int j,int[] nums, int target){
+        if(i>j){
+            return -1;
+        }
+        int mid = i + (j-i)/2;
+        if(i<=j && target < nums[mid]){
+            j=mid-1;
+            return rec(i,j,nums,target);
+        }
+        else if(i<=j && target >nums[mid]){
+            i=mid+1;
+            return rec(i,j,nums,target);
+        }
+        else if(i<=j && target == nums[mid]){
+            return mid;
+        }
+        return 0;
+    }
+
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+
+        int mid = i + (j-i)/2;
+
+        int result = rec(i,j,nums,target);
+
+        return result;
+    }
+}
